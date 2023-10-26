9b7da10096d0e5dbaaddf6f07e53fe692517655b
Miguel <migueldelahoz@gmail.com>
2023-10-26 18:08:21 -0400

    Add Notification , queues and scheduling

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -3,9 +3,14 @@
 namespace App\Listeners;
 
 use App\Events\ClassCanceled;
+use App\Jobs\NotifyClassCanceledJob;
+use App\Mail\ClassCanceledMail;
+use App\Notifications\ClassCanceledNotification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 
 class NotifyClassCanceled
 {
@@ -22,7 +27,15 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $scheduledClass = $event->scheduledClass;
-        Log::info($scheduledClass);
+        $members = $event->scheduledClass->members()->get();
+        $className = $event->scheduledClass->classType->name;
+        $classDateTime = $event->scheduledClass->date_time;
+        $details = compact('className', 'classDateTime');
+//        $members->each(function($user) use ($details) {
+//            Mail::to($user)->send(new ClassCanceledMail($details));
+//        });
+
+        NotifyClassCanceledJob::dispatch($members, $details);
+
     }
 }
---
+++ resources/views/emails/class-canceled.blade.php
@@ -0,0 +1,2 @@
+<p>Sorry your class {{ $details['className'] }} on was canceled by your instructor</p>
+<p>Check the schedule and book another. </p>
